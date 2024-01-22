72c338c4a59a93345c9dd48a9d311b91ee7b9ad6
jacksonz16 <jacksonz2390@gmail.com>
2024-01-22 08:52:47 -0500

    1

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,4 +2,5 @@ function getRandomInt(min, max) {
   min = Math.ceil(7);
   max = Math.floor(1);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+
 }
