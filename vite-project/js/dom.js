575d76b6b3136d79b9685f3ff32b62fff84b904f
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-04-30 14:22:16 -0400

    1

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -2,4 +2,7 @@ export const DOMselectors = {
     flipForm: document.getElementById("flip"),
     flipOptions: document.getElementById("options"),
     result: document.getElementById("result"),
+    menu: document.getElementById("menu"),
+    heads: document.getElementById("heads"),
+    tails: document.getElementById("tails"),
   };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,31 +2,53 @@ import '../css/style.css'
 import { DOMselectors } from './dom.js'
 import { coin } from './coins.js'
 
+let screenShow = 0
+let amountFlipped = [0,0]
+
 function flipcoin(coinfliped) {
   const flips = []
   for(let i = 0; i < coinfliped; i++) {
     const randomValue = coin[
       Math.floor(Math.random() * coin.length)
     ];
+    console.log(amountFlipped[randomValue.value - 1])
+    amountFlipped[randomValue.value - 1]++
+    DOMselectors.heads.textContent = `Heads: ${amountFlipped[0]}`
+    DOMselectors.tails.textContent = `Tails: ${amountFlipped[1]}`
     flips.push(randomValue);
-  };
+
+  };    
   return flips;
 };
 
+showPoints()
+
+addEventListener("keydown", (event) => {if(event.key == "Shift"){
+  showPoints()
+}});
+
+function showPoints(){
+  screenShow++
+  if(screenShow % 2){
+    DOMselectors.menu.style.display = "none"; 
+  }
+  else{
+    DOMselectors.menu.style.display = "block"; 
+  }
+}
 
 DOMselectors.flipForm.addEventListener("submit", function(event) {
   event.preventDefault();
   DOMselectors.result.innerHTML = "";
   const coinfliped = parseInt(DOMselectors.flipOptions.value);
   const result = flipcoin(coinfliped);
-  result.forEach(flip => {
+  result.forEach(coin => {
     DOMselectors.result.insertAdjacentHTML(
       "beforeend",
       `
-        <div class="coin">
-          <img src="${flip.img}" class="coin-image"><br>
-        </div>
+      <div class="coin">
+      <img src="${coin.img}" class="coin-image"><br>
+    </div>
       `
     );
-  });
-});
+  });})
